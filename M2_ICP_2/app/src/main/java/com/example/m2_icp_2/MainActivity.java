4fdf24def036d6bdd71b453f2ff4ba6a57427a5c
akkipant <44404812+akkipant@users.noreply.github.com>
2020-03-21 21:13:14 -0500

    Final Comit

---
+++ M2_ICP_2/app/src/main/java/com/example/m2_icp_2/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.m2_icp_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Boolean Valflag;
+    EditText user, pass;
+    Button button;
+    Intent login;
+
+
+    private View.OnClickListener signInOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            signInClicked();
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = new Button(this);
+
+        button = (Button) findViewById(R.id.signIn);
+
+        button.setOnClickListener(signInOnClickListener);
+
+    }
+
+
+    private void signInClicked() {
+
+        user = (EditText) findViewById(R.id.username);
+        pass = (EditText) findViewById(R.id.password);
+        Valflag = false;
+        login = new Intent(MainActivity.this, homeActivity.class);
+        String username = user.getText().toString();
+        String password = pass.getText().toString();
+        if(!username.isEmpty() && !password.isEmpty()) {
+            if(username.equals("User") && password.equals("Password")) {
+                Valflag = true;
+            }
+        }
+
+        if(Valflag) {
+            startActivity(login);
+        }
+
+        if(!Valflag) {
+            Toast.makeText(getApplicationContext(),"Details did not match",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
---
+++ M2_ICP_2/app/src/main/java/com/example/m2_icp_2/homeActivity.java
@@ -0,0 +1,42 @@
+package com.example.m2_icp_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class homeActivity extends AppCompatActivity {
+
+    Button button;
+    Intent logout;
+
+    private View.OnClickListener signOutOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            signOutClicked();
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        button = new Button(this);
+
+
+        button = (Button) findViewById(R.id.logout);
+
+        button.setOnClickListener(signOutOnClickListener);
+    }
+
+
+    private void signOutClicked() {
+        logout = new Intent(homeActivity.this, MainActivity.class);
+        //startActivity(logout);
+        finish();
+    }
+}
